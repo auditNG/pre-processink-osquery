61396698270e3786174ba7beec8409f36ed336e6
Ravi Honnavalli <ravihr@gmail.com>
2018-02-12 16:22:41 +0530

    Unmarshalling config and parsing out fields of interest from the input message

--- transform/fim_transformer.go
+++ transform/fim_transformer.go
@@ -3,56 +3,84 @@ package transform
 import (
   "fmt"
   "os"
-  "strconv"
-  "strings"
-  // "github.com/buger/jsonparser"
   "encoding/json"
 )
 
+const (
+  GREEN = 1 + iota
+  YELLOW
+  RED
+)
+
 func NewFIMTransformer() FIMTransformer {
-  return FIMTransformer{}
+  return FIMTransformer{
+    confObj: new(TransformConfig),
+  }
 }
 
-type FIMTransformer struct{}
+type FIMTransformer struct{
+  confObj *TransformConfig
+}
+
+func (f FIMTransformer) Init(config string) error {
+  err := json.Unmarshal([]byte(config), f.confObj)
+  if err != nil {
+        fmt.Println(err)
+  }
+  return err
+}
 
 func (f FIMTransformer) Process(message string, config string, outputFile *os.File) error {
+  // Read in config
+  err := f.Init(config)
+  if(err != nil) {
+    fmt.Println(err)
+    return nil
+  }
 
+  // Instantiate transform helper
   transformHelper := NewTransformHelper()
-  val, _ := f.getSyscalls(config)
-  fmt.Println(val)
 
-
+  //Get syscall
   syscall, err := transformHelper.GetIntValue(message, "syscall=")
 	if nil != err {
 		fmt.Println("Unable to get syscall")
+    return nil
 	}
-	fmt.Println("syscall: " + strconv.Itoa(syscall))
+  fmt.Println(syscall)
 
+  //Get exitcode
 	exitcode, err := transformHelper.GetIntValue(message, "exit=")
 	if nil != err {
 		fmt.Println("Unable to get exitcode")
+    return nil
 	}
-	fmt.Println("exitcode: " + strconv.Itoa(exitcode))
+  fmt.Println(exitcode)
+
+  //Get executable
+  executable, err := transformHelper.GetIntValue(message, "exe=")
+  if nil != err {
+    fmt.Println("Unable to get exitcode")
+    return nil
+  }
+  fmt.Println(executable)
+
+  //Get user
+  user, err := transformHelper.GetIntValue(message, "user=")
+	if nil != err {
+		fmt.Println("Unable to get exitcode")
+    return nil
+	}
+  fmt.Println(user)
 
 
   return nil
 }
 
-func (f FIMTransformer) getSyscalls(config string) ([]int, error) {
-  var confObj TransformConfig
-  err := json.Unmarshal([]byte(config), &confObj)
+func (f FIMTransformer) isSyscallsInList(syscall int) (bool) {
+    return false
+}
 
-    if err != nil {
-          fmt.Println("Unmarshall error")
-          fmt.Println(err)
-          return nil,nil
-    }
-    fmt.Printf("%+v\n", confObj)
-    fmt.Println(confObj.Fim.SyscallList);
-    fmt.Println(strings.Join(confObj.Fim.SyscallList, ", "))
-
-    marsh, _ := json.Marshal(confObj)
-    fmt.Println("Marshalled: " + string(marsh))
-    return nil, nil
+func (f FIMTransformer) applyLabelAlgo(syscall int, exitcode int, executable string, user int) {
 
 }
--- transform/transform_config.go
+++ transform/transform_config.go
@@ -4,7 +4,7 @@ type TransformConfig struct {
 		Fim struct {
 			AppGreylist []string
 			FileList    []string
-			SyscallList []string
+			SyscallList []int
 			FieldLists  []struct {
 				Pattern string
 				Type    string
