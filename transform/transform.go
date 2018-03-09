bb71df28e7b67cdff69e2bb701e9797418323df6
Arpit <arpit.waghmare@walmartlabs.com>
2018-03-09 16:14:24 +0530

    Structured message in map

--- transform/transform.go
+++ transform/transform.go
@@ -24,6 +24,8 @@ func (t Transform) Process(input string, outputFile *os.File) error {
 	jsonparser.ArrayEach([]byte(input),
 		func(actVal []byte, _ jsonparser.ValueType, _ int, err error) {
 			var message=string("")
+			var data map[string]string
+			data=make(map[string]string)
 			// jsonparser.ArrayEach(actVal, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 			// 	name,err:= jsonparser.GetString(value, "[1]")
 			// 	version,err:=jsonparser.GetString(value, "version")
@@ -34,35 +36,27 @@ func (t Transform) Process(input string, outputFile *os.File) error {
 			jsonparser.ObjectEach(actVal, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
       	//fmt.Printf("Key: '%s ' Value: '%s '\n", string(key), string(value))
 				message=message+string(key)+"="+string(value)+"\n"
+				data[string(key)]=string(value)
+
 				return nil
 					}, "_source", "osquery_distributed_query_result","result","[0]")
 			//fmt.Println(message)
-			t.extractMessage(message,outputFile)
+			for key,value:= range data{
+				fmt.Println("key:",key,"  value:",value)
+			}
+			//t.extractMessage(message,outputFile)
 			t.processMessage(message, outputFile)
-
-
-			// calendarTime, err := jsonparser.GetString(actVal, "_source", "osquery_distributed_query_result","result","[0]")
-			// if err != nil {
-			// 	fmt.Println("JSON parsing error: ", err)
-			// 	return
-			// }
-			// fmt.Println(calendarTime)
-
-			// if nil != err {
-			// 	return
-			// }
 
 		}, "hits", "hits")
 
 	return nil
 }
-func (t Transform) extractMessage(message string,outputFile *os.File) ([]string,error){
+//func (t Transform) extractMessage(message string,outputFile *os.File) ([]string,error){
 	//var content []string =strings.Fields(message)
-	var content []string =strings.Split(message,"\n")
-	fmt.Println("In extract message")
-	fmt.Println(content[0],content[1])
-	return content,nil
-}
+	//var content []string =strings.Split(message,"\n")
+
+	//return content,nil
+//}
 
 func (t Transform) processMessage(message string, outputFile *os.File) error {
 
