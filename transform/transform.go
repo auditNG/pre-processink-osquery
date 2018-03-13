d3a7fe04f9b4dd7b94da477da7877586ca906bc8
Arpit <arpit.waghmare@walmartlabs.com>
2018-03-13 21:52:45 +0530

    updated parsing result

--- transform/transform.go
+++ transform/transform.go
@@ -5,7 +5,7 @@ import (
 	"github.com/buger/jsonparser"
 	"io/ioutil"
 	"os"
-	"strings"
+
 )
 
 var spaceChar = byte('\n')
@@ -20,43 +20,27 @@ type Transform struct {
 
 func (t Transform) Process(input string, outputFile *os.File) error {
 
-
+var message=string("")
 	jsonparser.ArrayEach([]byte(input),
 		func(actVal []byte, _ jsonparser.ValueType, _ int, err error) {
-			var message=string("")
-			var data map[string]string
-			data=make(map[string]string)
-			// jsonparser.ArrayEach(actVal, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-			// 	name,err:= jsonparser.GetString(value, "[1]")
-			// 	version,err:=jsonparser.GetString(value, "version")
-			// 	fmt.Println(name,version)
-			// 		}, "_source", "osquery_distributed_query_result","result")
+
+			jsonparser.ArrayEach(actVal, func(value []byte, dataType jsonparser.ValueType, offset int,err error)  {
+				//fmt.Println(message[i])
+				jsonparser.ObjectEach(value,func(key []byte, pair []byte, dataType jsonparser.ValueType, offset int) error{
+					message=message+string(key)+"="+string(pair)+"\n"
+
+					return nil
+				})
+			
 
 
-			jsonparser.ObjectEach(actVal, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
-      	//fmt.Printf("Key: '%s ' Value: '%s '\n", string(key), string(value))
-				message=message+string(key)+"="+string(value)+"\n"
-				data[string(key)]=string(value)
+					}, "_source", "osquery_distributed_query_result","result")
+					fmt.Println(message)
 
-				return nil
-					}, "_source", "osquery_distributed_query_result","result","[0]")
-			//fmt.Println(message)
-			for key,value:= range data{
-				fmt.Println("key:",key,"  value:",value)
-			}
-			//t.extractMessage(message,outputFile)
-			t.processMessage(message, outputFile)
-
-		}, "hits", "hits")
-
+		}, "hits","hits")
+	t.processMessage(message,outputFile)
 	return nil
 }
-//func (t Transform) extractMessage(message string,outputFile *os.File) ([]string,error){
-	//var content []string =strings.Fields(message)
-	//var content []string =strings.Split(message,"\n")
-
-	//return content,nil
-//}
 
 func (t Transform) processMessage(message string, outputFile *os.File) error {
 
@@ -67,7 +51,6 @@ func (t Transform) processMessage(message string, outputFile *os.File) error {
 		fmt.Println("Error reading transform config")
 		return err
 	}
-
 	fimTransformer.Process(message, string(config), outputFile)
 
 	return nil
